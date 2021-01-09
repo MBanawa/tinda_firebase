156844be4bc75408de31776194590fedbf119d4d
MBanawa <mikebanawa25@gmail.com>
2021-01-09 19:12:47 +0800

    continue working on generate QR code UI

--- lib/screens/generate_qrcode.dart
+++ lib/screens/generate_qrcode.dart
@@ -1,7 +1,23 @@
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 
-class GenerateQr extends StatelessWidget {
+class GenerateQr extends StatefulWidget {
+  @override
+  _GenerateQrState createState() => _GenerateQrState();
+}
+
+class _GenerateQrState extends State<GenerateQr> {
+  var _qrController = TextEditingController();
+  String _qrCode;
+  FocusNode _qrFocus = FocusNode();
+
+  void _qrGenerator() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _qrCode = _qrController.text.trim();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,11 +28,21 @@ class GenerateQr extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            BarcodeWidget(
-              data: 'Animal Ka',
-              barcode: Barcode.qrCode(),
-              color: Colors.black,
-            ),
+            _qrCode == null
+                ? Container(
+                    child: Icon(
+                      Icons.qr_code,
+                      size: 200,
+                      color: Colors.teal.shade900,
+                    ),
+                  )
+                : BarcodeWidget(
+                    height: 200,
+                    width: 200,
+                    data: _qrCode,
+                    barcode: Barcode.qrCode(),
+                    color: Colors.black,
+                  ),
             SizedBox(height: 60),
             Padding(
               padding: const EdgeInsets.only(top: 8),
@@ -29,13 +55,11 @@ class GenerateQr extends StatelessWidget {
                 padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
                 margin: EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
                 child: TextField(
+                  focusNode: _qrFocus,
+                  controller: _qrController,
                   decoration: InputDecoration(
                     hintStyle: TextStyle(color: Colors.grey),
-                    hintText: 'Enter QR Code Name here',
-                    prefixIcon: Icon(
-                      Icons.qr_code,
-                      color: Colors.teal.shade800,
-                    ),
+                    hintText: 'Type your desired QR Code Name here',
                     border: InputBorder.none,
                   ),
                 ),
@@ -54,7 +78,8 @@ class GenerateQr extends StatelessWidget {
                     style: TextStyle(color: Colors.white),
                   ),
                   onPressed: () {
-                    Navigator.pop(context);
+                    // Navigator.pop(context);
+                    _qrGenerator();
                   }),
             ),
           ],
