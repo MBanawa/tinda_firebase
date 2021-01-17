c8a3c500b55356f9ba0beb725a41ff96bd41a592
MBanawa <mikebanawa25@gmail.com>
2021-01-17 09:21:14 +0800

    finished code to save qrlist to tinda folder in phone

--- lib/screens/myQrCode_screen.dart
+++ lib/screens/myQrCode_screen.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
 
 class MyQrCodeScreen extends StatefulWidget {
   @override
@@ -9,9 +16,12 @@ class MyQrCodeScreen extends StatefulWidget {
 
 class _MyQrCodeScreenState extends State<MyQrCodeScreen> {
   Stream userstream;
+  File _imageFile;
 
   CollectionReference qrcollection =
       FirebaseFirestore.instance.collection('qrData');
+
+  ScreenshotController screenshotController = ScreenshotController();
 
   @override
   void initState() {
@@ -29,52 +39,127 @@ class _MyQrCodeScreenState extends State<MyQrCodeScreen> {
     });
   }
 
-  //TODO: Print Gridview
+  Future<bool> _requestPermission(Permission permission) async {
+    if (await permission.isGranted) {
+      return true;
+    } else {
+      var result = await permission.request();
+      if (result == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Qr Codes'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.download_sharp),
+            onPressed: () {
+              Directory directory;
+              _requestPermission(Permission.storage);
+              try {
+                _imageFile = null;
+                screenshotController
+                    .capture(delay: Duration(milliseconds: 10))
+                    .then((File image) async {
+                  //print("Capture Done");
+                  setState(() {
+                    _imageFile = image;
+                  });
+
+                  if (await _requestPermission(Permission.storage)) {
+                    directory = await getExternalStorageDirectory();
+
+                    String newPath = '';
+                    List<String> folders = directory.path.split('/');
+
+                    ///storage/emulated/0/Android/data/com.banawa.tinda/files
+                    for (int x = 1; x < folders.length; x++) {
+                      String folder = folders[x];
+                      if (folder != 'Android') {
+                        newPath += '/' + folder;
+                      } else {
+                        break;
+                      }
+                    }
+                    newPath = newPath + '/Tinda';
+                    directory = Directory(newPath);
+                    print(directory.path);
+                  } else {
+                    return false;
+                  }
+                  if (!await directory.exists()) {
+                    await directory.create(recursive: true);
+                  }
+                  if (await directory.exists()) {
+                    final result = await ImageGallerySaver.saveImage(
+                        image.readAsBytesSync(),
+                        quality: 100);
+                    print("File Saved");
+                  }
+                }).catchError((onError) {
+                  print(onError);
+                });
+              } catch (e) {
+                print(e);
+              }
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 15),
-              child: StreamBuilder<QuerySnapshot>(
-                stream: userstream,
-                builder: (ctx, qrSnapshot) {
-                  if (qrSnapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  final qrDocs = qrSnapshot.data.docs;
-                  return GridView.builder(
-                    shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 2.5 / 3,
-                      crossAxisSpacing: 10,
-                    ),
-                    physics: BouncingScrollPhysics(),
-                    itemCount: qrDocs.length,
-                    itemBuilder: (context, index) {
-                      return GridTile(
-                        child: GestureDetector(
-                          onTap: () {
-                            //TODO: navigate to qr screen
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Image.network(qrDocs[index].data()['qrUrl']),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 0.0),
-                                  child: Text(
+        child: Screenshot(
+          controller: screenshotController,
+          child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.only(top: 15),
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: userstream,
+                  builder: (ctx, qrSnapshot) {
+                    if (qrSnapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    final qrDocs = qrSnapshot.data.docs;
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 2.5 / 3,
+                        crossAxisSpacing: 10,
+                      ),
+                      physics: BouncingScrollPhysics(),
+                      itemCount: qrDocs.length,
+                      itemBuilder: (context, index) {
+                        return GridTile(
+                          child: GestureDetector(
+                            onTap: () {
+                              //TODO: navigate to qr screen
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  BarcodeWidget(
+                                    data: qrDocs[index].data()['qrName'],
+                                    barcode: Barcode.qrCode(),
+                                    color: Colors.black,
+                                    height: 150,
+                                    width: 150,
+                                  ),
+                                  // Image.network(qrDocs[index].data()['qrUrl']),
+                                  Text(
                                     'Code: ${qrDocs[index].data()['qrName']}',
                                     style: TextStyle(
                                       color: Colors.teal.shade900,
@@ -84,18 +169,18 @@ class _MyQrCodeScreenState extends State<MyQrCodeScreen> {
                                     overflow: TextOverflow.ellipsis,
                                     maxLines: 4,
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            )
-          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
