22a65a859dfc47f57dc63dbd3236d1183474497c
MBanawa <mikebanawa25@gmail.com>
2021-03-22 20:07:57 +0800

    start fifo validator

--- lib/widgets/itemBuilder/edit_item.dart
+++ lib/widgets/itemBuilder/edit_item.dart
@@ -110,11 +110,13 @@ class _EditItemState extends State<EditItem> {
   }
 
   //deduct user entered quantity from the firstIn quantity
-  //TODO: what if the user enters an amount that is greater than the entire stock supply??
-  // create a validator that checks remainingQuantity versus items collection's quantity field
+
   void _firstOut() async {
     remainingQuantity = int.parse(_editItemQuantityController.text.trim());
 
+    //TODO: what if the user enters an amount that is greater than the entire stock supply??
+    // create a validator that checks remainingQuantity versus items collection's quantity field
+    // add if statement here to check:
     do {
       var desiredDocId = _removeDocId;
       await _firstIn();
