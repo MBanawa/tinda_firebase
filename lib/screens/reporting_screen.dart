3160b7833b66210919b5452fffbfdd9261b5486b
MBanawa <mikebanawa25@gmail.com>
2021-03-23 21:43:56 +0800

    test fl charts in reporting manager

--- lib/screens/reporting_screen.dart
+++ lib/screens/reporting_screen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tinda/widgets/charts/line_chart_widget.dart';
 
 class ReportingScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Reporting Screen'),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: Text('Reports Manager'), centerTitle: true),
+        body: PageView(
+          children: [
+            LineChartPage(),
+          ],
+        ),
+      );
+}
+
+class LineChartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+        color: const Color(0xff020227),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: LineChartWidget(),
+        ),
+      );
 }
