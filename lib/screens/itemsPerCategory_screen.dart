6e08ac207a1d56b44aa38114f3414e85fc89277f
MBanawa <mikebanawa25@gmail.com>
2021-03-24 19:41:47 +0800

    minor changes

--- lib/screens/itemsPerCategory_screen.dart
+++ lib/screens/itemsPerCategory_screen.dart
@@ -45,7 +45,7 @@ class _ItemsPerCategoryState extends State<ItemsPerCategory> {
     });
   }
 
-  //TODO: pre-load images so that it won't show grey shadow first? Look at my twitter history for answer
+  //TODO: pre-load images so that it won't show grey shadow first?
   @override
   Widget build(BuildContext context) {
     return Scaffold(
